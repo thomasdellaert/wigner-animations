350caff7ab5c33a0296b781b1862eff245b6fa28
Thomas Dellaert <dellaert.thomas@gmail.com>
2021-08-31 22:14:44 -0700

    changed colors of the arrows to reflect how they add

--- demos/3angularmomenta.py
+++ demos/3angularmomenta.py
@@ -21,11 +21,15 @@ scene.center = vec(0, 1, 0)
 make_axes(2)
 
 v1 = PrecessVector(j1, m1)
+v1.jarrow.color = color.red
 v2 = PrecessVector(j2, m2)
+v2.jarrow.color = color.green
 v3 = PrecessVector(j3, m3)
+v3.jarrow.color = color.blue
 v12 = CoupledVector(v1, v2, j12, m12)
-v12.jarrow.color = color.green
+v12.jarrow.color = color.yellow
 v123 = CoupledVector(v12, v3, j, m)
+v123.jarrow.color = color.white
 
 # attach_trail(v2.jarrow, color=color.cyan, type="curve", radius=0.01)
 
@@ -35,12 +39,16 @@ j1={j1}, j2={j2}, j3={j3}, j23={j23}, j={j}, m={m}"""
 scene2.center = vec(0, 1, 0)
 make_axes(2)
 
-v1_2 = PrecessVector(j1, m1)
-v2_2 = PrecessVector(j2, m2)
-v3_2 = PrecessVector(j3, m3)
-v23_2 = CoupledVector(v2_2, v3_2, j23, m23)
-v23_2.jarrow.color = color.blue
-v123_2 = CoupledVector(v1_2, v23_2, j, m)
+w1 = PrecessVector(j1, m1)
+w1.jarrow.color = color.red
+w2 = PrecessVector(j2, m2)
+w2.jarrow.color = color.green
+w3 = PrecessVector(j3, m3)
+w3.jarrow.color = color.blue
+w23 = CoupledVector(w2, w3, j23, m23)
+w23.jarrow.color = color.cyan
+w123 = CoupledVector(w1, w23, j, m)
+w123.jarrow.color = color.white
 
 dt = 0.01
 while True:
@@ -48,5 +56,5 @@ while True:
     v123.rotate(v123.omega * dt)
     v123.twist(dt)
 
-    v123_2.rotate(v123_2.omega * dt)
-    v123_2.twist(dt)
+    w123.rotate(w123.omega * dt)
+    w123.twist(dt)
